01f4e11db302411f024b3b6f6801d192210c2ee6
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2018-01-31 12:54:59 +0100

    Typo

--- ios/RNOpenTokEventEmitter.m
+++ ios/RNOpenTokEventEmitter.m
@@ -22,7 +22,7 @@ RCT_EXPORT_MODULE();
              @"onArchiveStartedWithId",
              @"onArchiveStoppedWithId",
              @"onSessionDidBeginReconnecting",
-             @"onSessionDidReconnect"];
+             @"onSessionDidReconnect",
              @"onVideoEnabled",
              @"onVideoDisabled"];
 }
