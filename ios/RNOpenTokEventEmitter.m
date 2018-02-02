e9cf9ba28c5e272b54ef5ddad85c641eef4a6d91
dskoneczny <damian.skoneczny@razorbear.com>
2018-02-02 10:32:20 +0100

    Update RNOpenTokEventEmitter.m

--- ios/RNOpenTokEventEmitter.m
+++ ios/RNOpenTokEventEmitter.m
@@ -24,6 +24,7 @@ RCT_EXPORT_MODULE();
              @"onSessionDidBeginReconnecting",
              @"onSessionDidReconnect",
              @"onVideoEnabled",
+             @"onPublishCameraError",
              @"onVideoDisabled"];
 }
 
